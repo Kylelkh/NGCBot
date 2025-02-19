b65b67e7870d3c0c6ee50e804a1c5014075ebe90
云云 <52269948+ngc660sec@users.noreply.github.com>
2025-02-19 16:05:05 +0800

    更新获取好友昵称的逻辑
    先试试这个方案

--- BotServer/BotFunction/InterfaceFunction.py
+++ BotServer/BotFunction/InterfaceFunction.py
@@ -1,13 +1,10 @@
 import FileCache.FileCacheServer as Fcs
 import xml.etree.ElementTree as ET
-import Config.ConfigServer as Cs
 from OutPut.outPut import op
 import requests
 import time
 import os
 
-
-getIdNameInt = 0
 
 def getWithdrawMsgData(content):
     """
@@ -69,30 +66,47 @@ def getAtData(wcf, msg):
     return atUserLists, noAtMsg.strip()
 
 
-def getIdName(wcf, Id=None, roomId=None):
+def getIdName(wcf, Id=None, roomId=None, retry=0, max_retries=3):
     """
     获取好友或者群聊昵称
+    :param wcf: 微信框架对象
+    :param Id: 用户ID
+    :param roomId: 群聊ID
+    :param retry: 当前重试次数
+    :param max_retries: 最大重试次数
     :return:
     """
     try:
-        global getIdNameInt
-        name_list = wcf.query_sql("MicroMsg.db",
-                                  f"SELECT UserName, NickName FROM Contact WHERE UserName = '{Id}';")
-        if not name_list:
-            return getIdName(wcf, Id, roomId)
+        name_list = wcf.query_sql("MicroMsg.db", f"SELECT UserName, NickName FROM Contact WHERE UserName = '{Id}';")
+
+        if not name_list and retry < max_retries:
+            # 如果查询结果为空且未达到最大重试次数，则等待一秒后重试
+            time.sleep(1)
+            return getIdName(wcf, Id, roomId, retry + 1, max_retries)
+        elif not name_list:
+            # 达到最大重试次数但仍无法获取数据，返回原始ID
+            op(f'[~]: 获取好友或者群聊昵称出现错误, 错误信息: 查询结果为空')
+            return Id
+
         name = name_list[0]['NickName']
-        getIdNameInt += 1
-        if getIdNameInt == 4:
-            getIdNameInt = 0
-            if '@chatroom' not in Id:
-                return wcf.get_alias_in_chatroom(Id, roomId)
-            else:
-                return roomId
-        return name
+
+        if '@chatroom' not in Id:
+            nickName = wcf.get_alias_in_chatroom(Id, roomId)
+            if not nickName:
+                return Id
+            return nickName
+        else:
+            return name
+
     except Exception as e:
         op(f'[~]: 获取好友或者群聊昵称出现错误, 错误信息: {e}')
-        time.sleep(1)
-        return getIdName(wcf, Id, roomId)
+        if retry < max_retries:
+            # 如果发生异常且未达到最大重试次数，则等待一秒后重试
+            time.sleep(1)
+            return getIdName(wcf, Id, roomId, retry + 1, max_retries)
+        else:
+            # 达到最大重试次数但仍无法获取数据，返回原始ID
+            return Id
 
 
 
